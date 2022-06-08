dc8ea537d9a13a82a7c562ee98040f0c8340dccd
Nichjohn123 <njohn4586@example.com>
2022-06-08 04:39:53 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -23,11 +23,14 @@ int main(void)
 
 
 		sum = fib1 + fib2;
+
 		printf("%lu", sum);
 
 
 		fib1 = fib2;
+
 		fib2 = sum;
+
 
 
 		if (count == 49)
@@ -38,8 +41,8 @@ int main(void)
 
 			printf(",");
 
-	
 	}
+
 
 
 
