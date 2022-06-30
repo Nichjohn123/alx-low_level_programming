b3e7980ab8877540d308e3eb3db0cb258e547502
Nichjohn123 <njohn4586@gmail.com>
2022-06-30 07:57:15 -0700

    task000

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,19 +1,18 @@
 #include "main.h"
-
+#include <stdlib>
 /**
  * malloc_checked - array for prints a string
  * @b: number of memory
+ *
  * Return: void
+ * if malloc fails, status value is equall to 98.
  */
-
 void *malloc_checked(unsigned int b)
 {
-	void *p;
-	P = malloc(b);
+	char *p;
 
+	p = malloc(b);
 	if (p == NULL)
-	{
 		exit(98);
-	}
 	return (p);
 }
