c9c21a01974d8f98a7484ab0921a69d100460d52
Nichjohn123 <njohn4586@gmail.com>
2022-07-14 07:30:53 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,8 +4,8 @@
 #include "lists.h"
 
 /**
- * print_list - check the code for Holberton school students.
- * @h: name of the nodes.
+ * print_list - chaeck the code for Holberton School students.
+ * @h: name of the list
  * Return: the number of nodes.
  */
 size_t print_list(const list_t *h)
